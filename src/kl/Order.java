f54badc63e25d14465e198dcba1040ef3588f783
Micheal001412 <88073875+Micheal001412@users.noreply.github.com>
2021-09-10 20:48:36 +0800

    Micheal

---
+++ src/kl/Order.java
@@ -0,0 +1,202 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kl;
+import java.util.Date;
+import java.util.Scanner;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author user
+ */
+public class Order {
+    private DeliveryClass delivery;
+    private Register register;
+    private Cart cart;
+    private Order order;
+    private String orderId;
+    private static Double amount;
+    private Double deliveryFee;
+    protected Date dateCreated;
+    private Double subTotal;
+  
+    public Order(){
+      
+    }
+    
+    public Order(String orderId,Double amount,Double subTotal){
+        this.orderId = orderId;
+        this.amount = amount;
+        this.delivery=delivery;
+        this.register=register;
+        this.cart=cart;
+        this.order=order;
+        this.subTotal=subTotal;
+        dateCreated = new Date();
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public DeliveryClass getDelivery() {
+        return delivery;
+    }
+
+    public void setDelivery(DeliveryClass delivery) {
+        this.delivery = delivery;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String OrderId) {
+        this.orderId = OrderId;
+    }
+
+    public Double getSubTotal() {
+        return subTotal;
+    }
+
+    public void setSubTotal(Double subTotal) {
+        this.subTotal = subTotal;
+    }
+    
+    public Double getAmount() {
+        return amount;
+    }
+
+    public Double getDeliveryFee() {
+        return deliveryFee;
+    }
+
+    public void setDeliveryFee(Double deliveryFee) {
+        this.deliveryFee = deliveryFee;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+    
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+    
+    public void autoCreateOrderId(Order order, String OrderId)
+    { 
+     order.setOrderId("");
+       if (order.getOrderId()== "") {
+                            order.setOrderId("O1000");
+                            
+                        } else {
+                            OrderId = order.getOrderId();
+                            int id, num;
+                            OrderId = OrderId.substring(1, 5);
+                            id = (Integer.parseInt(OrderId)) + 1;
+                            order.setOrderId("O" + id);
+ 
+                        }
+             String query = "Select * from Order WHERE OrderId ='" + OrderId + "DESC LIMIT 1";
+             System.out.println("\nOrder ID:"+order.getOrderId());
+    }
+        
+    public void CalSubTotal(int discount,Cart cart,Order order,Register register,DeliveryClass delivery,String cartList){
+          
+            try{
+              
+                  cartList = cartList.substring(1, cartList.length()-1);           //remove curly brackets
+                  String[] keyValuePairs = cartList.split(">");              //split the string to creat key-value pairs
+                  Map<String,String> map = new HashMap<>();
+
+              for(String pair : keyValuePairs)                        //iterate over the pairs
+             {
+                 String[] entry = pair.split("=");                   //split the pairs to get key and value 
+                 map.put(entry[0].trim(), entry[1].trim());          //add them to the hashmap and trim whitespaces
+             }
+                cart.setSubTotalPrice(Double.parseDouble(map.get("subTotalPrice")));
+                System.out.println("\n-----------------------------------------");   
+                System.out.println("|The Amount:           |RM"+cart.getSubTotalPrice()+"\t\t|");
+           
+           deliveryFee=order.deliveryFee(delivery, register, "Johor");
+           
+           amount=cart.getSubTotalPrice();
+             if(amount<=200.00){
+                discount=0;
+             }
+             else if(amount>=500 && amount<=1000)
+             {
+                discount=20;
+             }
+             else{
+               discount=50;
+             }
+               System.out.print("\n|Discount:             |RM"+discount+"\t\t|");
+               subTotal=amount+deliveryFee-discount;
+               System.out.println("\n-----------------------------------------"); 
+               System.out.print("|SubTotal:             |RM"+subTotal+"\t\t|");
+               System.out.println("\n-----------------------------------------");  
+          }
+          catch(Exception ex){
+            System.out.print(ex);
+          }
+    }
+    
+    public Double deliveryFee(DeliveryClass delivery,Register register,String states){
+      if(register.getStates()==states){
+         System.out.print("|Delivery Fee:         |RM"+delivery.calculateState()+"\t\t|");
+         
+      }
+      return delivery.calculateState();
+    }
+    
+//    public void runInsertQuery(Connection con,String newOrderId,String newCustomerId,String newCustomerName,String newCustomerContent,String newCustomerStates,String newProductName,int newQuantity,Double newAmount,Double newDelivery,Double newDiscount,Double newSubTotal){        
+//        this.orderId.add(newOrderId);
+//        this.productDetail.add(newProductDetail);
+//        this.
+//        stringorderId = String.join(",", this.orderId);
+//        stringTotalPrice = totalPrice.stream().map(Object::toString).collect(Collectors.joining(","));
+//        stringQuantity = quantity.stream().map(Object::toString).collect(Collectors.joining(","));
+//        String insertNewSql = "INSERT INTO Order(CartId, ProductName, TotalPrice, Quantity, SubTotalPrice)" + "VALUES (?,?,?,?,?)";
+//        try {
+//        Statement insertStmt = con.createStatement();
+//        PreparedStatement preparedStatement = con.prepareStatement(insertNewSql);   
+//        preparedStatement.setString(1, this.cartId);
+//        preparedStatement.setString(2, stringProductName);
+//        preparedStatement.setString(3, stringTotalPrice);
+//        preparedStatement.setString(4, stringQuantity);
+//        preparedStatement.setDouble(5, subTotalPrice);
+//        preparedStatement.executeUpdate();   
+//        } catch(SQLException ex) {
+//            ex.printStackTrace();
+//        }
+//    }
+}
